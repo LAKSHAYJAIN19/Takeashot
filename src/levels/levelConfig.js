e864743607344f9d1b51086ed07eaac426617d62
LAKSHAYJAIN19 <72019776+LAKSHAYJAIN19@users.noreply.github.com>
2026-02-20 23:53:54 +0530

    Update levelConfig.js

--- src/levels/levelConfig.js
+++ src/levels/levelConfig.js
@@ -4,7 +4,7 @@ import targetMedium from "../assets/target_medium.png";
 import targetHard from "../assets/target_hard.png";
 import targetExpert from "../assets/target_expert.png";
 import bowf from "../assets/bowff.png";
-import lefthand from "../assets/lefthandd.png"
+import lefthand from "../assets/lefthand.png"
 import righthand from "../assets/righthand.png"
 import scope from "../assets/bscope.png";
 import bowstring from "../assets/bowstring.svg"
